6149073f7fd19df0f720e681f731bcf4c10e06ae
headbwoi <tochukwujohn24@gmail.com>
2022-04-19 11:20:12 +0100

    finished styling

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    fontFamily: {
+      'mono': ["'Space Mono'", 'monospace']
+    },
+    extend: {
+      colors: {
+        StrongCyan: "hsl(172, 67%, 45%)",
+        black: "black",
+        veryDarkCyan: "hsl(183, 100%, 15%)",
+        darkGrayishCyan: "hsl(186, 14%, 43%)",
+        grayishCyan: "hsl(184, 14%, 56%)",
+        lightGrayishCyan: "hsl(185, 41%, 84%)",
+        veryLightGrayishGyan: "hsl(189, 41%, 97%)",
+        white: "hsl(0, 0%, 100%)",
+      },
+    },
+  },
+  plugins: [],
+};
