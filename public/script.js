afef76b137836d10794d950cc727a7359e0bb71b
headbwoi <tochukwujohn24@gmail.com>
2022-04-20 14:28:38 +0100

    stuck in javaScript

--- public/script.js
+++ public/script.js
@@ -1,8 +1,35 @@
-const billEl = document.querySelector("#bill")
-const btnGroup = document.querySelector("#btn-group")
-const buttonEl = btnGroup.querySelectorAll("button")
-const noOfPeople = document.querySelector("#no-of-people")
-const tipAmount = document.querySelector("#tip-amount")
-const totalAmount = document.querySelector("#total-amount")
+const billEl = document.querySelector("#bill");
+const btnGroup = document.querySelector("#btn-group"),
+  buttonEl = btnGroup.querySelectorAll("button");
+const customEl = document.querySelector("#custom");
+const noOfPeople = document.querySelector("#no-of-people");
+const tipAmount = document.querySelector("#tip-amount");
+const totalAmount = document.querySelector("#total-amount");
+const errorTxt = document.querySelector("#error");
 
+buttonEl.forEach((item) => {
+  item.addEventListener("click", () => {
+    calculateTip();
+  });
+});
 
+customEl.addEventListener("input", () => {
+  calculateTip();
+});
+
+noOfPeople.addEventListener("input", () => {
+  calculateTip();
+});
+
+function calculateTip() {
+  let percent = parseInt(buttonEl);
+  console.log(percent);
+  let bill = billEl.value;
+  let numberOfPeople = noOfPeople.value;
+
+  let tipAmountCalc = bill * (percent / 100) * numberOfPeople;
+  let tipTotalCalc = bill / numberOfPeople + tipAmountCalc;
+
+  tipAmount.textContent = `$${tipAmountCalc}`;
+  totalAmount.textContent = `$${tipTotalCalc}`;
+}
