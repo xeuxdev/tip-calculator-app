0ea5c6591ed6144d7e35f5e81c99957874aab997
headbwoi <tochukwujohn24@gmail.com>
2022-04-20 13:37:28 +0100

    blah blah blah

---
+++ public/script.js
@@ -0,0 +1,8 @@
+const billEl = document.querySelector("#bill")
+const btnGroup = document.querySelector("#btn-group")
+const buttonEl = btnGroup.querySelectorAll("button")
+const noOfPeople = document.querySelector("#no-of-people")
+const tipAmount = document.querySelector("#tip-amount")
+const totalAmount = document.querySelector("#total-amount")
+
+
