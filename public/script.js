0aad64c44b33e0b35a3859339e611d901431e868
headbwoi <tochukwujohn24@gmail.com>
2022-04-21 15:10:31 +0100

    added active states for resetbtn and finished the javaScript functionality

--- public/script.js
+++ public/script.js
@@ -6,30 +6,61 @@ const noOfPeople = document.querySelector("#no-of-people");
 const tipAmount = document.querySelector("#tip-amount");
 const totalAmount = document.querySelector("#total-amount");
 const errorTxt = document.querySelector("#error");
+const resetBtn = document.querySelector("#reset-btn")
 
-buttonEl.forEach((item) => {
-  item.addEventListener("click", () => {
-    btnvalue = parseInt(item.value)
-    console.log(btnValue)
-    calculateTip();
-  });
-});
+let tipPercentage = 15
+let billAmount = 0
+let no_Of_people = 0
 
-customEl.addEventListener("input", () => {
-  calculateTip();
-});
 
-noOfPeople.addEventListener("input", () => {
-  calculateTip();
-});
+function removeActiveTipClass(){
+  buttonEl.forEach((percent) => {
+    percent.classList.remove("active")
+  })
+}
 
 function calculateTip() {
-  let bill = billEl.value;
-  let numberOfPeople = noOfPeople.value;
+  if(tipPercentage <= 0 || billAmount <= 0 || no_Of_people <= 0) return
 
-  let tipAmountCalc = bill * (percent / 100) * numberOfPeople;
-  let tipTotalCalc = bill / numberOfPeople + tipAmountCalc;
+  const tips = (tipPercentage / 100) * billAmount
+   let tip_Amount = tips / no_Of_people
+  let tip_Total = billAmount / no_Of_people + tip_Amount
+  tipAmount.textContent = `$${tip_Amount.toFixed(2)}`
+  totalAmount.textContent = `$${tip_Total.toFixed(2)}`
+  resetBtn.classList.add("active")
 
-  tipAmount.textContent = `$${tipAmountCalc}`;
-  totalAmount.textContent = `$${tipTotalCalc}`;
 }
+
+buttonEl.forEach((percent) => {
+  percent.addEventListener("click", () => {
+    removeActiveTipClass()
+    percent.classList.add("active")
+    tipPercentage = Number(percent.value)
+    calculateTip()
+  })
+})
+
+customEl.addEventListener("input", (val) => {
+  removeActiveTipClass()
+  tipPercentage = Number(val.target.value)
+  calculateTip()
+})
+
+billEl.addEventListener("input", (val) => {
+  billAmount = Number(val.target.value)
+  calculateTip()
+})
+
+noOfPeople.addEventListener("input", (val) => {
+  no_Of_people = Number(val.target.value)
+  calculateTip()
+})
+
+resetBtn.addEventListener("click", () => {
+  billEl.value = ""
+  noOfPeople.value = ""
+  removeActiveTipClass()
+  tipAmount.textContent = "$0.00"
+  totalAmount.textContent = "$0.00"
+  resetBtn.classList.remove("active")
+})
